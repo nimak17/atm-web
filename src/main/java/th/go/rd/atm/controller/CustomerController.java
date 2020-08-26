1729df936c4ba14f5fa7663d88886d583c57befa
P. Kampan <panvaleek@gmail.com>
2020-08-26 14:17:32 +0700

    Add form to create data in table

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,6 +2,9 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -9,14 +12,25 @@ import java.util.ArrayList;
 @Controller
 public class CustomerController {
 
-    @RequestMapping("/customer") //URL
+    private ArrayList<Customer> customers = new ArrayList<>();
+
+    @GetMapping("/customer")
+    //@RequestMapping("/customer") //URL
     public String getCustomerPage(Model model) {
-        ArrayList<Customer> customers = new ArrayList<>();
-        customers.add(new Customer(1, "Peter", "1234"));
-        customers.add(new Customer(2, "Nancy", "2345"));
-        customers.add(new Customer(3, "Rick", "3456"));
+//        ArrayList<Customer> customers = new ArrayList<>();
+//        customers.add(new Customer(1, "Peter", "1234"));
+//        customers.add(new Customer(2, "Nancy", "2345"));
+//        customers.add(new Customer(3, "Rick", "3456"));
         model.addAttribute("allCustomers", customers);
         return "customer"; // customer.html
     }
+
+    @PostMapping("customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) { //@ModelAttribute <- หมายถึงรับมาจากหน้าบ้าน
+        customers.add(customer);
+        model.addAttribute("allCustomers", customers);
+        return "redirect:customer"; //redirect <- รีเซ็ตค่าใน form ก่อนส่งค่าไป
+    }
+
 
 }
